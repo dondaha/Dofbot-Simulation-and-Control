5775265e2a8fa82a7880930a4a1bd2a77b758738
Wenhua Wu <2142431167wwh@sjtu.edu.cn>
2024-09-07 20:02:11 +0800

    first_commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+from dofbot import DofbotEnv
+import numpy as np
+import time
+
+
+
+if __name__ == '__main__':
+    env = DofbotEnv()
+    env.reset()
+    Reward = False
+    while not Reward:
+
+        jointPoses = np.zeros(5)
+        gripperAngle = 0
+        '''
+        #获取物块位姿、目标位置和机械臂位姿，计算机器臂关节和夹爪角度，使得机械臂夹取绿色物块，放置到紫色区域。
+        '''
+
+        env.dofbot_control(jointPoses,gripperAngle)
+        Reward = env.reward()
+
