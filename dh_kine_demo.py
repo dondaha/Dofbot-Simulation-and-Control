a21de3c6986bc0294341b8cc26733841d74b1455
E-T-E-R-N-A-L-B-L-U-E <31281844+E-T-E-R-N-A-L-B-L-U-E@users.noreply.github.com>
2024-09-19 20:57:17 +0800

    add homework part1 and part2

--- dh_kine_demo.py
+++ dh_kine_demo.py
@@ -1,5 +1,6 @@
 import roboticstoolbox as rtb
 import numpy as np
+import math
 
 pi = 3.1415926          # 定义pi常数
 
@@ -35,21 +36,114 @@ dofbot = rtb.DHRobot(
 print(dofbot)
 
 
-target_pos = np.array([
+'''
+Part1 给出一下关节姿态时的机械臂正运动学解，并附上仿真结果
+0.(demo) [0., pi/3, pi/4, pi/5, 0.]
+1.[pi/2, pi/5, pi/5, pi/5, pi]
+2.[pi/3, pi/4, -pi/3, -pi/4, pi/2]
+3.[-pi/2, pi/3, -pi/3*2, pi/3, pi/3]
+'''
+
+# part1 demo
+fkine_input0 = [0., pi/3, pi/4, pi/5, 0.]
+fkine_result0 = dofbot.fkine(fkine_input0)
+print(fkine_result0)
+dofbot.plot(q=fkine_input0, block=True)
+
+# part1-1
+fkine_input1 = [pi/2, pi/5, pi/5, pi/5, pi]
+fkine_result1 = dofbot.fkine(fkine_input1)
+print(fkine_result1)
+dofbot.plot(q=fkine_input1, block=True)
+
+# part1-2
+fkine_input2 = [pi/3, pi/4, -pi/3, -pi/4, pi/2]
+fkine_result2 = dofbot.fkine(fkine_input2)
+print(fkine_result2)
+dofbot.plot(q=fkine_input2, block=True)
+
+# part1-3
+fkine_input3 = [-pi/2, pi/3, -pi/3*2, pi/3, pi/3]
+fkine_result3 = dofbot.fkine(fkine_input3)
+print(fkine_result3)
+dofbot.plot(q=fkine_input3, block=True)
+
+
+'''
+Part1 给出一下关节姿态时的机械臂逆运动学解，并附上仿真结果
+0.(demo) 
+    [
+        [-1., 0., 0., 0.1,],
+        [0., 1., 0., 0.],
+        [0., 0., -1., -0.1],
+        [0., 0., 0., 1.]
+    ]
+1.
+    [
+        [1., 0., 0., 0.1,],
+        [0., 1., 0., 0.],
+        [0., 0., 1., 0.1],
+        [0., 0., 0., 1.]
+    ]
+2.
+    [
+        [cos(pi/3), 0., -sin(pi/3), 0.05,],
+        [0., 1., 0., 0.03],
+        [sin(pi/3), 0., cos(pi/3)., -0.1],
+        [0., 0., 0., 1.]
+    ]
+3.
+    [
+        [-0.866, -0.25, -0.433, -0.03704,],
+        [0.5, -0.433, -0.75, -0.06415],
+        [0., -0.866, 0.5, 0.3073],
+        [0., 0., 0., 1.]
+    ]
+'''
+
+#part2 demo
+target_pos0 = np.array([
     [-1., 0., 0., 0.1,],
     [0., 1., 0., 0.],
     [0., 0., -1., -0.1],
     [0., 0., 0., 1.]
 ])
+ikine_result0 = dofbot.ik_LM(target_pos0)[0]
+print("ikine: ", np.array(ikine_result0) / 3.14 * 180.)
+dofbot.plot(q=ikine_result0, block=True)
 
-# 输出机器人逆运动学demo
-ikine_result = dofbot.ik_LM(target_pos)[0]
-print("ikine: ", np.array(ikine_result) / 3.14 * 180.)
+
+#part2-1
+target_pos1 = np.array([
+    [1., 0., 0., 0.1,],
+    [0., 1., 0., 0.],
+    [0., 0., 1., 0.1],
+    [0., 0., 0., 1.]
+])
+ikine_result1 = dofbot.ik_LM(target_pos1)[0]
+print("ikine: ", np.array(ikine_result1) / 3.14 * 180.)
+dofbot.plot(q=ikine_result1, block=True)
 
 
-# 输出机器人正运动学demo
-fkine_result = dofbot.fkine(ikine_result)
-print("fkine: ", fkine_result)
+#part2-2
+target_pos2 = np.array([
+    [math.cos(-pi/3), 0., -math.sin(-pi/3), 0.2,],
+    [0., 1., 0., 0.],
+    [math.sin(-pi/3), 0., math.cos(-pi/3), 0.2],
+    [0., 0., 0., 1.]
+])
+ikine_result2 = dofbot.ik_LM(target_pos2)[0]
+print("ikine: ", np.array(ikine_result2) / 3.14 * 180.)
+dofbot.plot(q=ikine_result2, block=True)
 
-# 展示机器人正运动学demo
-dofbot.plot(q=ikine_result, block=True)
+
+#part2-3
+target_pos3 = np.array([
+    [-0.866, -0.25, -0.433, -0.03704,],
+    [0.5, -0.433, -0.75, -0.06415],
+    [0., -0.866, 0.5, 0.3073],
+    [0., 0., 0., 1.]
+])
+ikine_result3 = dofbot.ik_LM(target_pos3)[0]
+print("ikine: ", np.array(ikine_result3) / 3.14 * 180.)
+dofbot.plot(q=ikine_result3, block=True)
